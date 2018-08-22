9f20560704721d83a81e9cdfeb49754b964d4ec7
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-08-22 22:26:34 +0300

    app skeleton has been added

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express                   = require('express');
+const bodyParser                = require('body-parser');
+const databaseCon               = require('./dbConnection');
+const config                    = require('./config');
+//const router                    = require('./router');
+
+
+databaseCon.then((dbConnection)=>{
+    console.log('Database connection is Successfully.');
+    global.__db__ = dbConnection;
+
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.use((request, rsponse, next)=>{
+        request.__db__ = dbConnection;
+        next();
+    });
+
+    //router(app);
+
+    app.listen(config.server.port, () => {
+        console.log(`Server is started at http://localhost:${config.server.port}`);
+    });
+
+}).catch((error) => {
+    console.log("Database connection is fail.", error);
+});
---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    server: {
+        port: 3000
+    },
+    database: {
+        url: "mongodb://localhost:27017/event-calendar"
+    }
+};
