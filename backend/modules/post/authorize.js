f0e5299c6d7161b88b86a893de0eb587d1af2ae5
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-08-23 01:26:56 +0300

    authorize post controller has been fixed

--- backend/modules/post/authorize.js
+++ backend/modules/post/authorize.js
@@ -7,17 +7,15 @@ module.exports = (request, response)=> {
 
     request.body.password = crypto.createHash('md5').update(request.body.password).digest("hex");
 
-    Users.find({
+    Users.findOne({
         login: request.body.login,
         password: request.body.password
-    }, (error, result) => {
+    }, (error, user) => {
 
-        if (0 == result.length) {
+        if (null === user) {
             response.status(403).send({error: 403, message: 'Forbidden'});
             return;
         }
-
-        let user = result[0];
 
         Sessions.insert(user._id).then((session) => {
             response.json(session);
